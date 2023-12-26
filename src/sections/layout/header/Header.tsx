37e39051efd74154429603db0d7e746ab7f9311a
Samir Rodas <serodas9710@gmail.com>
2023-12-26 15:09:31 -0500

    Add media queries

--- src/sections/layout/header/Header.tsx
+++ src/sections/layout/header/Header.tsx
@@ -1,52 +1,67 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import styles from "./Header.module.scss";
 
 export const Header = () => {
+	const [toggleMenu, setToggleMenu] = useState(true);
+	const handleMenu = (toggleMenu: boolean) => {
+		setToggleMenu(!toggleMenu);
+	};
+
 	return (
 		<>
 			<header className={styles.header}>
-				<nav>
+				<button className={styles.menu__btn} onClick={() => handleMenu(toggleMenu)}>
+					<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 448 512">
+						<path
+							fill="#4ba598"
+							d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
+						/>
+					</svg>
+				</button>
+				<nav className={toggleMenu ? styles.nav : ""}>
 					<ul>
-						<li className="nav">
-							<NavLink to="/" className={styles.home}>
+						<li>
+							<NavLink to="/" className={styles.home} onClick={() => handleMenu(toggleMenu)}>
 								Inicio
 							</NavLink>
 						</li>
-						<li className="nav">
-							<NavLink to="/education" className={styles.education}>
+						<li>
+							<NavLink
+								to="/education"
+								className={styles.education}
+								onClick={() => handleMenu(toggleMenu)}
+							>
 								Educación
 							</NavLink>
 						</li>
-						<li className="nav">
-							<NavLink to="/experience" className={styles.experience}>
+						<li>
+							<NavLink
+								to="/experience"
+								className={styles.experience}
+								onClick={() => handleMenu(toggleMenu)}
+							>
 								Experiencia
 							</NavLink>
 						</li>
-						<li className="nav">
-							<NavLink to="/projects" className={styles.projects}>
+						<li>
+							<NavLink
+								to="/projects"
+								className={styles.projects}
+								onClick={() => handleMenu(toggleMenu)}
+							>
 								Proyectos
 							</NavLink>
 						</li>
-						<li className="nav">
-							<NavLink to="/contact" className={styles.contact}>
+						<li>
+							<NavLink
+								to="/contact"
+								className={styles.contact}
+								onClick={() => handleMenu(toggleMenu)}
+							>
 								Contáctame
 							</NavLink>
-						</li>
-						<li>
-							<button className={styles.menu__btn}>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									height="24"
-									width="24"
-									viewBox="0 0 448 512"
-								>
-									<path
-										fill="#4ba598"
-										d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
-									/>
-								</svg>
-							</button>
 						</li>
 					</ul>
 				</nav>
