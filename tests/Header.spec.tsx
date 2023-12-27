69cbfa9c7ae3312aae3494f7d1d9c5e3f4a39576
Samir Rodas <serodas9710@gmail.com>
2023-12-27 16:34:53 -0500

    Fix header test

--- tests/Header.spec.tsx
+++ tests/Header.spec.tsx
@@ -1,11 +1,12 @@
 import { screen } from "@testing-library/react";
 
+import { lightTheme } from "../src/domain/Theme";
 import { Header } from "../src/sections/layout/header/Header";
 import { renderWithRouter } from "./RenderWithRouter";
 
 describe("Header section", () => {
 	it("show all list items", async () => {
-		renderWithRouter(<Header />);
+		renderWithRouter(<Header theme={lightTheme} />);
 		await screen.findByRole("link", {
 			name: "Inicio",
 		});
