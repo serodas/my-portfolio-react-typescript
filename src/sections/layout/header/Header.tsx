0b2fa9b1c30dc727041560ce48cbb04605faac8c
Samir Rodas <serodas9710@gmail.com>
2023-12-29 16:20:40 -0500

    Fix icon close in mobile

--- src/sections/layout/header/Header.tsx
+++ src/sections/layout/header/Header.tsx
@@ -1,4 +1,4 @@
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faClose, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
@@ -32,12 +32,13 @@ export const Header = ({
 		<>
 			<header className={styles.header}>
 				<button className={styles.menu__btn} onClick={() => handleMenu(toggleMenu)}>
-					<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 448 512">
-						<path
-							fill="#4ba598"
-							d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
-						/>
-					</svg>
+					<figure>
+						{toggleMenu ? (
+							<FontAwesomeIcon icon={faBars} size="2x" style={{ color: "#4ba598" }} />
+						) : (
+							<FontAwesomeIcon icon={faClose} size="2x" style={{ color: "#4ba598" }} />
+						)}
+					</figure>
 				</button>
 				<nav className={toggleMenu ? styles.nav : ""}>
 					<ul>
